81a096e9fedd10ff157e9d314e951359ef08529d
ariadnaGomez <ariadna.gomez@soprasteria.com>
2018-03-13 12:40:21 +0100

    chore: add sass, router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-view name="menu"/>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import { Loading } from 'element-ui'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+const router = new Router({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    components: {
+      default: HelloWorld
     }
-  ]
+  }]
 })
+let loadingPage
+router.beforeEach((to, from, next) => {
+  loadingPage = Loading.service({
+    fullscreen: true
+  })
+  next()
+})
+
+router.afterEach((to, from) => {
+  loadingPage.close(
+})
+
+export default router
