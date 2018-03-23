1f1bd31dbb51706e7fa1c019cd50b67e8c1fbbd4
Arcas791 <Arcas791@gmail.com>
2018-03-23 09:25:39 +0100

    fix index route when logged

--- src/components/target.vue
+++ src/components/target.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="card">
-    <div class="card-header">
+    <div
+      class="card-header"
+      @mouseover="prueba(true)">
       <img
         src="https://www.gcfaprendelibre.org/files/personage/image/11/R_p.png">
-      <h3>NOmbre</h3>
+      <h3>NOmbreeeeeee</h3>
     </div>
-    <div class="card-header test">
-      <img
-        src="https://www.gcfaprendelibre.org/files/personage/image/11/R_p.png">
-      <h3>NOmbre</h3>
+    <div
+      v-show="cardHeaderOptions"
+      class="card-header hidden"
+      @mouseleave="prueba(false)">
+      <i class="el-icon-edit">Assign To</i>
+      <i class="el-icon-edit">Edit card</i>
     </div>
     <p class="description">#487(0.5) Añadir botón Ver ficha de producto)</p>
   </div>
@@ -16,7 +20,21 @@
 
 <script>
 export default {
-  name: 'Target'
+  name: 'Target',
+  data () {
+    return {
+      cardHeaderOptions: false
+    }
+  },
+  methods: {
+    prueba (shouldBeHidden) {
+      if (shouldBeHidden) {
+        this.cardHeaderOptions = true
+      } else {
+        this.cardHeaderOptions = false
+      }
+    }
+  }
 }
 </script>
 
@@ -36,6 +54,22 @@ export default {
       h3{
         margin: 0;
         margin-left: 15px;
+      }
+    }
+    .hidden{
+      justify-content: space-between;
+      background-color: white;
+      position: absolute;
+      top: 0;
+      width: 100%;
+      opacity: 0.9;
+      height: 50%;
+      i{
+        padding: 15px;
+        font-size: 15px;
+      }
+      i:hover{
+        color: green;
       }
     }
     .description{
